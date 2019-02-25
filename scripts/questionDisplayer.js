f259bfc5b84b84aa2c0370b1a6e3f3941e297a21
Ian Price <ianpriceve@gmail.com>
2019-02-24 18:30:45 -0700

    set up qestionDisplayer file

---
+++ scripts/questionDisplayer.js
@@ -0,0 +1,23 @@
+const questionDisplayer = (function() {
+
+  //Cache DOM
+
+  //Bind Events
+
+  //Event Listeners
+  pubsub.subscribe('levelStart', level);
+
+  function render(){
+
+  };
+
+  function selection(){
+
+  };
+
+  function displayAnswers(){
+
+  };
+
+
+})();
