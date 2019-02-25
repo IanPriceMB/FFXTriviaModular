6f30e992d870ad973344945fc9699627a6e08c3e
Ian Price <ianpriceve@gmail.com>
2019-02-24 19:21:42 -0700

    added timer.js, made answerSelection function in qd

--- scripts/questionDisplayer.js
+++ scripts/questionDisplayer.js
@@ -3,34 +3,43 @@
 const questionDisplayer = (function() {
 
   let questionTracker = 0;
+ 
 
   //Cache DOM
   const $container = $(`.container`);
 
   //Bind Events
+  $container.on('click', '.answer', answerSelection);
 
   //Event Listeners
   pubsub.subscribe('levelStart', render);
 
   function render(level){
     $container.empty();
+
+    $container.append(`<div class='question'>${Questions[level][questionTracker].question}`);
     for (let i = 0; i < Questions[level][questionTracker].answers.length; i++){    
       const answer = $("<div class='answer'>");
       answer.attr("id", i);
       answer.attr("data-value", Questions[level][questionTracker].answers[i].value);
       $container.append(answer);     
-      const answerID = $(`<span class='answerID'>${i}</span>`);
-      const answerText = $(`<span class='answer'>${Questions[level][questionTracker].answers[i].a}</span>`);
+      const answerID = $(`<span class='ID'>${i}</span>`);
+      const answerText = $(`<span class='text'>${Questions[level][questionTracker].answers[i].a}</span>`);
       answer.append(answerID, answerText);
   }
   };
 
-  function selection(){
-
+  function answerSelection(level){
+    questionTracker++;
+    if($(this).attr('data-value') == 1){
+      render(level);
+    } else {
+      displayAnswers();
+    }
   };
 
   function displayAnswers(){
-
+    console.log('woot');
   };
 
 
